e5c60b1400e3919086d04c8dfb686bf809133d2b
Joseph Watson <joseph@cloud-team.com>
2023-11-01 22:06:12 +0000

    Fix linter complaints

--- formate.go
+++ formate.go
@@ -6,6 +6,6 @@ func (d Date) Format(s string) string {
 	return d.In(time.UTC).Format(s)
 }
 
-func (d DateTime) Format(s string) string {
-	return d.In(time.UTC).Format(s)
+func (dt DateTime) Format(s string) string {
+	return dt.In(time.UTC).Format(s)
 }
